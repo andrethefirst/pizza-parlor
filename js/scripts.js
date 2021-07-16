14bfd99c51244c0122166554b66474f985810c15
Andre Velasquez <andrethefirst21@gmail.com>
2021-07-16 15:13:13 -0700

    add pizzaOrder constructor and prototype

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,12 @@
 //Business Logic for Pizza-----
 
-function PizzaOrder() {
-  this.PizzaOrder
-
+function PizzaOrder(size, topping, secondtopping) {
+  this.size = size;
+  this.topping = topping;
+  this.secondtopping =secondtopping;
 }
+
+PizzaOrder.prototype.chooseSize = function() {
+  return this.size + " " + this.topping + " " +this.secondtopping;
+};
+
